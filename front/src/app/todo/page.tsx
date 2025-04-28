cc3bd6cccdd9b998ab46584e9d8abfe59fcf6193
kaneko1117 <yuta_kaneko@pj100.biz>
2025-04-28 16:51:51 +0900

    疎通

--- front/src/app/todo/page.tsx
+++ front/src/app/todo/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { TodoList } from "@/features/TodoList";
 import { TodoData } from "@/features/TodoList/type";
 
-export default async function Todo() {
+const client = new ApolloClient({
+  uri: "http://localhost:8080/query",
+  cache: new InMemoryCache(),
+});
+
+export default function Todo() {
   const TEST_DATA: TodoData = [
     { id: "1", task: "test1", checked: false },
     { id: "2", task: "test2", checked: true },
@@ -9,5 +17,10 @@ export default async function Todo() {
     { id: "4", task: "test4", checked: true },
     { id: "5", task: "test5", checked: false },
   ];
-  return <TodoList data={TEST_DATA} />;
+
+  return (
+    <ApolloProvider client={client}>
+      <TodoList data={TEST_DATA} />/
+    </ApolloProvider>
+  );
 }
--- front/src/features/TodoList/index.tsx
+++ front/src/features/TodoList/index.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { gql, useQuery } from "@apollo/client";
 import { TodoData } from "./type";
 import { TodoListForm } from "./components/TodoListForm";
 import { TodoCheckBox } from "./components/TodoCheckBox";
@@ -6,7 +9,21 @@ type Props = {
   data: TodoData;
 };
 
+const TodosQuery = gql(`
+  query GetTasks($id : ID!) {
+    getTasks(id: $id) {
+      task
+    }
+  }
+`);
+
 export const TodoList = ({ data }: Props) => {
+  const { data: graphql } = useQuery(TodosQuery, {
+    variables: {
+      id: 1,
+    },
+  });
+  console.log("graphql", graphql);
   return (
     <div className="flex flex-col items-center gap-5 h-screen mt-12">
       <div className="w-[500px]">
